8042d5f152b66b4df0ace05cc372053627244b53
JoaoMarcelo22 <joao_marcelo.22@hotmail.com>
2023-09-16 00:47:42 -0300

    feat: array e loop

---
+++ Modulo-1/aulaQuatro.js
@@ -0,0 +1,36 @@
+// array
+
+const alunos = ['joao','jose','maria'];
+
+console.log(alunos[0]);
+
+alunos.push('Renan'); // ultima posição 
+alunos.pop() // remove ultimo elemento
+alunos.shift() // remove primeiro elemento
+
+alunos[4] = 'vini';
+
+// repeticao for
+
+const notas =[];
+
+notas.push(5);
+notas.push(7);
+notas.push(8);
+notas.push(2);
+notas.push(5);
+notas.push(8);
+
+let notaFinal = 0;
+
+for (let index = 0; index < notas.length; index++) {
+    notaFinal += notas[index]
+}
+const media = notaFinal / notas.length
+
+const nome = 'joao marcelo da silva ferreira'
+
+for (let i = 0; i < nome.length; i++) {
+    //console.log(nome[i]);
+}
+console.log(media)
