9d99db371a33a99c66d74a0cecb05d653f01b701
JoaoMarcelo22 <joao_marcelo.22@hotmail.com>
2023-09-20 21:53:46 -0300

    feat: finalizando o primeiro modulo

--- Modulo-1/importacao/exercicio.js
+++ Modulo-1/importacao/exercicio.js
@@ -1,11 +1,55 @@
-const {nota,print} = require('./funcoesAuxiliares');
+// exercicio - 1
+const {nota,print,numero,salario} = require('./funcoesAuxiliares');
 
 let media = nota();
 
 if(media < 5){
-    print("Reprovado");
+    //print("Reprovado");
 }else if (media >= 5 && media < 7){
-    print("Recuperação");
+    //print("Recuperação");
 }else if (media >= 7 && media <= 10){
-    print("Aprovado");
+    //print("Aprovado");
 }else print("Valor invalido");
+
+// exercicio - 2
+
+const numeros = numero();
+
+let maiorPar = null;
+let menorImpar = null;
+
+for (let i = 0; i < numeros; i++) {
+    const numeros2 = numero();
+    if(numeros2 % 2 === 0 && numeros2 > maiorPar){
+        maiorPar = numeros2
+    } else if (menorImpar === null){
+        menorImpar = numeros2
+    } else if (numeros2 < menorImpar){
+        menorImpar = numeros2
+    }
+}
+//print(`Maior Par = ${maiorPar}, Menor Impar = ${menorImpar}`);
+
+
+// exercicio - 2
+
+const salarioBruto = salario();
+const salarioBeneficios = salario();
+
+function impostos(valor){
+    let impostos = 0;
+    if(valor > 0 && valor < 1100){
+        impostos = 0.05;
+    } else if (valor >= 1100 && valor < 2500){
+        impostos = 0.10;
+    } else if (valor >= 2500){
+        impostos = 0.15;
+    }
+    return impostos;
+}
+function valorTotal(valor,beneficio){
+    const salarioTotal = (valor - valor * impostos(valor)) + beneficio;
+    return salarioTotal;
+
+}
+print(`O total a ser recebido é = ${valorTotal(salarioBruto,salarioBeneficios)}`)
--- Modulo-1/importacao/funcoesAuxiliares.js
+++ Modulo-1/importacao/funcoesAuxiliares.js
@@ -21,4 +21,24 @@ function nota(){
     return valor;
 }
 
-module.exports = { gets,print,nota};
+// exercicio - 2
+numeros2 = [5,3,4,1,10,8];
+let i2 = 0;
+
+function numero(){
+    const valor2 = numeros2[i2];
+    i2++;
+    return valor2;
+}
+
+// exercicio - 3 
+entrada = [2000,250];
+let i3 = 0;
+
+function salario(){
+    const valor = entrada[i3];
+    i3++;
+    return valor;
+}
+
+module.exports = { gets,print,nota,numero,salario};
